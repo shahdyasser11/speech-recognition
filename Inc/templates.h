590b8ec2453b9791a50ccff85dd08f0d938fc90b
habiba marwan <habibamarwan698@gmail.com>
2026-05-08 23:14:40 +0300

    modified templates.h

--- Inc/templates.h
+++ Inc/templates.h
@@ -1,21 +1,26 @@
 #ifndef TEMPLATES_H
 #define TEMPLATES_H
 
-#define NUM_WORDS 2
+#define NUM_WORDS 8
 #define NUM_FEATURES 3
 
 const float word_templates[NUM_WORDS][NUM_FEATURES] = {
-    {1.000000, -1.000000, 1.000000},  /* word 0: "on"  */
-    {-1.000000, 1.000000, -1.000000}, /* word 1: "off" */
-
-    //  ON – OFF – START – STOP – LEFT – RIGHT – UP – DOWN
-    //   0 -  1  -  2  -   3   -   4   -   5   -  6 -   7
+    {0.814560, -0.897284, 0.827227},
+    {-0.858860, 0.959785, -0.851174},
+    {-0.196290, 0.914206, -0.243385},
+    {-1.240249, 1.104639, -1.226368},
+    {0.104041, 0.449637, 0.116269},
+    {0.594355, -0.257495, 0.583655},
+    {-1.083889, -0.310657, -1.079386},
+    {1.866334, -1.962832, 1.873163},
 };
 
 const float feature_mean[NUM_FEATURES] = {
-    0.064738, 0.256171, 0.050952};
+    0.071147, 0.247137, 0.055601
+};
 
 const float feature_std[NUM_FEATURES] = {
-    0.020025, 0.050160, 0.016271};
+    0.016716, 0.045781, 0.014049
+};
 
 #endif
